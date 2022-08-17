2185841d949dd7c6d0b41c70188a3fe3a5afdffe
RanjanGitHubb <96113106+RanjanGitHubb@users.noreply.github.com>
2022-08-17 15:25:18 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,6 +1,6 @@
 node {
   stage('Clone') {
-  git "https://github.com/RanjanGitHubb/demo.git"
+  git 'https://github.com/RanjanGitHubb/demo.git'
   }
   stage('Build') {
     bat "cd initial && mvn clean install"
